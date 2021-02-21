3d79313de9bc721b0559862979015df86365373e
Awesome-Q27 <dltjdrb01277@naver.com>
2021-02-21 18:25:53 +0900

    JS: Home contact button

--- main.js
+++ main.js
@@ -4,8 +4,8 @@
 const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
-    console.log(window.scrollY);
-    console.log(`navbarHeight: ${navbarHeight}`);
+    // console.log(window.scrollY);
+    // console.log(`navbarHeight: ${navbarHeight}`);
     if (window.scrollY > navbarHeight) {
         navbar.classList.add('navbar--scroll');
     } else {
@@ -15,15 +15,26 @@ document.addEventListener('scroll', () => {
 
 
 // Handle scrolling when tapping on the navbar menu
+function scrollIntiView(selector) {
+    var scrollTo = document.querySelector(selector); // 문자열을 가지는 element 탐색
+    scrollTo.scrollIntoView({behavior: 'smooth'});
+}
+
 const navbarMenu = document.querySelector('.navbar__menu');
 navbarMenu.addEventListener('click', (event) => {
-    const target = event.target;
-    const link = target.dataset.link; // 지정한 문자열이 들어 있다
+    const link = event.target.dataset.link; // 지정한 문자열이 들어 있다
     if (link == null) {
         return;
     }
 
     console.log(link);
-    const scrollTo = document.querySelector('#'+link); // 문자열을 가지는 element 탐색
-    scrollTo.scrollIntoView({behavior: 'smooth'});
+    scrollIntiView(link);
 });
+
+// Handle click on "Contact me" button on home
+const contackMeBtn = document.querySelector('.home__contact');
+contackMeBtn.addEventListener('click', () => {
+    console.log('#contact');
+    scrollIntiView('#contact');
+});
+
