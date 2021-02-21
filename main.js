4148e6e4298db04ee15b7f96928e090b20345316
Awesome-Q27 <dltjdrb01277@naver.com>
2021-02-21 18:12:07 +0900

    JS : navbar menu click

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
 'user strict';
 
+// Make navbar transparent when it is on the top
 const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
-
-// Make navbar transparent when it is on the top
 document.addEventListener('scroll', () => {
     console.log(window.scrollY);
     console.log(`navbarHeight: ${navbarHeight}`);
@@ -12,4 +11,19 @@ document.addEventListener('scroll', () => {
     } else {
         navbar.classList.remove('navbar--scroll');
     }
-})
+});
+
+
+// Handle scrolling when tapping on the navbar menu
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (event) => {
+    const target = event.target;
+    const link = target.dataset.link; // 지정한 문자열이 들어 있다
+    if (link == null) {
+        return;
+    }
+
+    console.log(link);
+    const scrollTo = document.querySelector('#'+link); // 문자열을 가지는 element 탐색
+    scrollTo.scrollIntoView({behavior: 'smooth'});
+});
