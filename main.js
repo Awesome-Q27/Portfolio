4bc3fcff254a6e8f506de8f8b16448caa74f2510
Awesome-Q27 <dltjdrb01277@naver.com>
2021-02-21 19:32:52 +0900

    JS: Project filter

--- main.js
+++ main.js
@@ -65,6 +65,29 @@ arrowUp.addEventListener("click", () => {
 });
 
 
+// Projects
+const workBtnContainer = document.querySelector('.work__caregories');
+const projectContainer = document.querySelector('.work__projects');
+const projects = document.querySelectorAll('.project');
+workBtnContainer.addEventListener('click', (e) => {
+    const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
+    if (filter == null) {
+        return;
+    }
+    projectContainer.classList.add('animation');
+    setTimeout(() => {
+        projects.forEach((project) => {
+            if (filter === '*' || filter === project.dataset.type) {
+                project.classList.remove('invisible');
+            }
+            else {
+                project.classList.add('invisible');
+            }
+        });
+        projectContainer.classList.remove('animation');   
+    }, 300); // 300ms 후에 {} 코드 실행
+});
+
 
 
 
