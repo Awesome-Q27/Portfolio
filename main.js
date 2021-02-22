2524fbef06a6378ef75367164ee11e27132409bc
Awesome-Q27 <dltjdrb01277@naver.com>
2021-02-22 16:33:35 +0900

    finish

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 'user strict';
 
-// 함수 정의
+// Scrolling Function
 function scrolling(selector) {
     var target = document.querySelector(selector); // 문자열을 가지는 element 탐색'
     // 절대좌표를 이용한 이동
@@ -9,11 +9,13 @@ function scrolling(selector) {
         top: absoluteTop - 50,
         behavior: 'smooth'
     });
+    selectNavItem(navItems[sectionIds.indexOf(selector)]);
     // 엘리멘트 자체로 이동
     // target.scrollIntoView({behavior: 'smooth'});
 }
 
 
+// ============================== Navbar ============================== 
 // Make navbar transparent when it is on the top
 const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
@@ -44,7 +46,52 @@ navbarToggleBtn.addEventListener('click', () => {
     navbarMenu.classList.toggle('open');
 });
 
+// Navbar Menu active
+const sectionIds = ['#home', '#about', '#skills', '#work', '#testimonials', '#contact'];
+const sections = sectionIds.map(id => document.querySelector(id));
+const navItems = sectionIds.map(id => document.querySelector(`[data-link="${id}"]`));
+let selectedNavItem = navItems[0];
+let selectedNavIndex = 0;
 
+const observerCallback = (entries, observer) => {
+    entries.forEach(entry => {
+        if(!entry.isIntersecting && entry.intersectionRatio > 0) {
+            const index = sectionIds.indexOf(`#${entry.target.id}`);
+            // 페이지가 올라옴
+            if(entry.boundingClientRect.y < 0) {
+                selectedNavIndex = index + 1;
+            }
+            else {
+                selectedNavIndex = index - 1;
+            }
+        }
+    })
+}
+const observerOptions = {
+    root: null,
+    rootmargin: '0px',
+    threshold: 0.3,
+};
+const myObserver = new IntersectionObserver(observerCallback, observerOptions);
+sections.forEach(section => myObserver.observe(section));
+
+function selectNavItem(selected) {
+    selectedNavItem.classList.remove('active');
+    selectedNavItem = selected;
+    selectedNavItem.classList.add('active');
+}
+
+window.addEventListener('wheel', () => {
+    if (window.scrollY === 0) {
+        selectedNavIndex = 0;
+    } else if (window.scrollY + window.innerHeight === document.body.clientHeight) {
+        selectedNavIndex = navItems.length - 1;
+    }
+    selectNavItem(navItems[selectedNavIndex]);
+})
+
+
+// ============================== Home ============================== 
 // Handle click on "Contact me" button on home
 const contackMeBtn = document.querySelector('.home__contact');
 contackMeBtn.addEventListener('click', () => {
@@ -59,23 +106,9 @@ document.addEventListener('scroll', () => {
     home.style.opacity = 1 - window.scrollY / homeheight;
 });
 
-// Show "arrow up" button when scrolling down
-const arrowUp = document.querySelector('.arrow-up');
-document.addEventListener('scroll', () => {
-    if (window.scrollY > homeheight / 2) {
-        arrowUp.classList.add('visible');
-    } else {
-        arrowUp.classList.remove('visible');
-    }
-});
 
-// Handle click on "arrow up" btn
-arrowUp.addEventListener("click", () => {
-    scrollIntiView('#home');
-});
-
-
-// Projects
+// ============================== Projects ============================== 
+// Filtering
 const workBtnContainer = document.querySelector('.work__caregories');
 const projectContainer = document.querySelector('.work__projects');
 const projects = document.querySelectorAll('.project');
@@ -105,6 +138,18 @@ workBtnContainer.addEventListener('click', (e) => {
 });
 
 
+// ============================== Arrow Up ============================== 
+// Show "arrow up" button when scrolling down
+const arrowUp = document.querySelector('.arrow-up');
+document.addEventListener('scroll', () => {
+    if (window.scrollY > homeheight / 2) {
+        arrowUp.classList.add('visible');
+    } else {
+        arrowUp.classList.remove('visible');
+    }
+});
 
-
-
+// Handle click on "arrow up" btn
+arrowUp.addEventListener("click", () => {
+    scrolling('#home');
+});
